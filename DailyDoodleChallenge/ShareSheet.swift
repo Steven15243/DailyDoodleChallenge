8f2f3734091c32287d54c9f8bf78a1bee7b48160
Steven15243 <vojst201@gmail.com>
2024-05-29 17:01:02 +0200

    Fixed Share Function

---
+++ DailyDoodleChallenge/ShareSheet.swift
@@ -0,0 +1,18 @@
+//
+//  ShareSheet.swift
+//  DailyDoodleChallenge
+//
+//  Created by Stefan on 29/05/2024.
+//
+
+import SwiftUI
+
+struct ShareSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareSheet()
+}
