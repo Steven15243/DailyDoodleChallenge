7f4ad25f1a881e8870b579fe39e324ed57b48b33
Steven15243 <vojst201@gmail.com>
2024-05-29 17:13:17 +0200

    Added feature to save pictures on the app

---
+++ DailyDoodleChallenge/SavedDoodlesView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedDoodlesView.swift
+//  DailyDoodleChallenge
+//
+//  Created by Stefan on 29/05/2024.
+//
+
+import SwiftUI
+
+struct SavedDoodlesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedDoodlesView()
+}
---
+++ DailyDoodleChallenge/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  DailyDoodleChallenge
+//
+//  Created by Stefan on 29/05/2024.
+//
+
+import Foundation
